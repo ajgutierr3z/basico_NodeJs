708a1e90ef29b72623f79960dcf6ef2225fc814b
ajgutierrez <84606504+ajgutierr3z@users.noreply.github.com>
2022-11-28 16:31:08 -0600

    code that print two letter

---
+++ 19.imprenta/imprenta.js
@@ -0,0 +1,16 @@
+/**
+ Codigo que permite imprimir letras de tipo imprenta
+**/
+
+console.log("aaaaaaaaaaaaaaaaaa");
+console.log("         a       a");
+console.log("         a       a");
+console.log("         a       a");
+console.log("         a       a");
+console.log("aaaaaaaaaaaaaaaaaa");
+console.log("jjjjjjjjj");
+console.log("j");
+console.log("j");
+console.log("j");
+console.log("j");
+console.log("jjjjjjjjjjjjjjjjjj");
